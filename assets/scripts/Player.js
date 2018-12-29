c7c6b461c6066aa4559c1eb0ceea2f38f828c15f
duyk16 <duyk16@gmail.com>
2018-12-29 18:17:27 +0700

    Catch star game

---
+++ assets/scripts/Player.js
@@ -0,0 +1,112 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // main character's jump height
+        jumpHeight: 0,
+        // main character's jump duration
+        jumpDuration: 0,
+        // maximal movement speed
+        maxMoveSpeed: 0,
+        // acceleration
+        accel: 0,
+        // jumping sound effect resource
+        jumpAudio: {
+            default: null,
+            type: cc.AudioClip
+        },
+    },
+
+    setJumpAction: function () {
+        // jump up
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // jump down
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // add a callback function to invoke other defined methods after the action is finished
+        var callback = cc.callFunc(this.playJumpSound, this);
+        //  repeat
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+    },
+
+    playJumpSound: function () {
+        // invoke sound engine to play the sound
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+
+    onKeyDown (event) {
+        // set a flag when key pressed
+        switch(event.keyCode) {
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.left:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+            case cc.macro.KEY.right:
+                this.accRight = true;
+                break;
+        }
+    },
+
+    onKeyUp (event) {
+        // unset a flag when key released
+        switch(event.keyCode) {
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.left:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+            case cc.macro.KEY.right:
+                this.accRight = false;
+                break;
+        }
+    },
+
+    onLoad: function () {
+        // initialize jump action
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        // Acceleration direction switch
+        this.accLeft = false;
+        this.accRight = false;
+        // The main character's current horizontal velocity
+        this.xSpeed = 0;
+
+        // Initialize the keyboard input listening
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);   
+    },
+
+    onDestroy () {
+        // Cancel keyboard input monitoring
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+    update: function (dt) {
+        // update speed of each frame according to the current acceleration direction
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // restrict the movement speed of the main character to the maximum movement speed
+        if ( Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // update the position of the main character according to the current speed
+        this.node.x += this.xSpeed * dt;
+    },
+});
